653634568c1e5e39e6bea4c8b954a391327be6ae
shinbobo <sbg5561@gmail.com>
2023-02-22 17:22:24 +0900

    swagger setup

---
+++ src/main/java/com/example/project/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.example.project.config;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+
+@EnableWebMvc
+@Configuration
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.project.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Api Documentation")
+                .description("swagger config")
+                .version("1.0")
+                .build();
+    }
+}
